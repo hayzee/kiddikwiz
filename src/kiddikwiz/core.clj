2c1e3edf5977e815ef0ef08afa0b6b73d5881771
hayzee <hayzee@users.noreply.github.com>
2021-01-20 19:57:01 +0000

    Added code comments

--- src/kiddikwiz/core.clj
+++ src/kiddikwiz/core.clj
@@ -1,38 +1,49 @@
 (ns kiddikwiz.core
   (:gen-class))
 
-(defn input [prompt]
+(def questions
+     "A bunch of questions"
+     (for [x (range 1 13)
+           y (range 1 13)]
+          {:x x
+           :y y
+           :question       (str x " * " y " = ")
+           :correct-answer (str (* x y))
+           :correct        nil
+           :time-taken     0}))
+
+(defn input
+  "Accept user input with a prompt"
+  [prompt]
   (do
     (print prompt)
     (flush)
     (read-line)))
 
-(defn datediff [d1 d2]
+(defn datediff
+  "Difference (in seconds) between two dates"
+  [d1 d2]
   (double (/ (- (.getTime d2) (.getTime d1)) 1000)))
 
-(def questions
-  (for [x (range 1 13)
-        y (range 1 13)]
-    {:x x
-    :y y
-    :question       (str x " * " y " = ")
-    :correct-answer (str (* x y))
-    :correct        nil
-    :time-taken     0}))
-
-(defn ask-question [q]
+(defn ask-question
+  "Ask a question and assoc the response"
+  [question]
   (let [tstart (java.util.Date.)
-        a (input (q :question))
+        a (input (question :question))
         t (datediff tstart (java.util.Date.))]
     (->
-      q
-      (assoc :correct (= a (q :correct-answer)))
+      question
+      (assoc :correct (= a (question :correct-answer)))
       (assoc :time-taken t))))
 
-(defn ask-questions [qseq]
+(defn ask-questions
+  "Ask a bunch of questions"
+  [qseq]
   (doall (map ask-question qseq)))
 
-(defn show-results [answers]
+(defn show-results
+  "Show the results of a round of questions"
+  [answers]
   (intern *ns* 'answers answers) ;; todo - get rid
   (let [{correct true incorrect false} (group-by :correct answers)
          num_answers (count answers)
@@ -43,7 +54,9 @@
     (println (format "Total time taken %.1f seconds" total_time))
     (println (format "Average time per question %.1f seconds" (/ total_time num_answers)))))
 
-(defn round[questions n]
+(defn round
+  "Ask a round of `n questions from `questions"
+  [questions n]
   (->>
     (shuffle questions)
     (take n)
@@ -51,12 +64,14 @@
     show-results))
 
 (defn play []
+  "Main play loop"
   (let [choice (input "Ready? [Y-Yes N-Exit] : ")]
     (if (#{"Y" "y"} choice)
       (do (round questions 10) (recur))
       (println "See ya, wouldn't wanna be ya!"))))
 
 (defn -main
+  "Main entry point"
  [& args]
   (play))
 
