3d4d98187fa954a1ec78e6d15898c9282ccbd20c
hayzee <hayzee@users.noreply.github.com>
2021-01-21 00:11:53 +0000

    Added 'answer analysis'

--- src/kiddikwiz/core.clj
+++ src/kiddikwiz/core.clj
@@ -5,12 +5,21 @@
   "A bunch of questions"
   (for [x (range 1 13)
         y (range 1 13)]
-    {:x x
-    :y y
-    :question       (str x " * " y " = ")
-    :correct-answer (str (* x y))
-    :correct        nil
-    :time-taken     0}))
+      {:no  nil
+       :x   x
+       :y   y
+       :question       (str x " * " y " = ")
+       :correct-answer (str (* x y))
+       :answer         nil
+       :correct        nil
+       :time-taken     nil}))
+
+(defn select-questions [questions n]
+  "Select n random questions, number them"
+  (->>
+    (shuffle questions)
+    (take n)
+    (map-indexed (fn [n m] (assoc m :no (inc n))))))
 
 (defn input
   "Accept user input with a prompt"
@@ -28,18 +37,26 @@
 (defn ask-question
   "Ask a question and assoc the response"
   [question]
-  (let [tstart (java.util.Date.)
-        a (input (question :question))
-        t (datediff tstart (java.util.Date.))]
-    (->
-      question
-      (assoc :correct (= a (question :correct-answer)))
-      (assoc :time-taken t))))
+  (let [tstart     (java.util.Date.)
+        answer     (input (str "Question " (:no question) " : " (:question question)))
+        time-taken (datediff tstart (java.util.Date.))]
+    (assoc question
+      :answer  answer
+      :correct (= answer (question :correct-answer))
+      :time-taken time-taken)))
 
 (defn ask-questions
   "Ask a bunch of questions"
   [qseq]
   (doall (map ask-question qseq)))
+
+(defn show-incorrect
+  [answers]
+  (if-let [wrong (seq (filter (complement :correct) answers))]
+    (do
+      (println "You answered the following questions incorrectly:")
+      (doseq [w wrong]
+             (println (str "Question " (w :no) ": " (w :question) (w :correct-answer) ", you answered " (w :answer)))))))
 
 (defn show-results
   "Show the results of a round of questions"
@@ -49,16 +66,18 @@
          num_answers (count answers)
          num_correct (count correct)
          total_time (reduce + (map :time-taken answers))]
+    (println "================================================")
     (println (format "You scored %s out of %d" num_correct num_answers))
     (println (format "Total time taken %.1f seconds" total_time))
-    (println (format "Average time per question %.1f seconds" (/ total_time num_answers)))))
+    (println (format "Average time per question %.1f seconds" (/ total_time num_answers)))
+    (show-incorrect answers)
+    (println "================================================")))
 
 (defn round
   "Ask a round of `n questions from `questions"
   [questions n]
   (->>
-    (shuffle questions)
-    (take n)
+    (select-questions questions n)
     ask-questions
     show-results))
 
@@ -66,7 +85,10 @@
   "Main play loop"
   (let [choice (input "Ready? [Y-Yes N-Exit] : ")]
     (if (#{"Y" "y"} choice)
-      (do (round questions 10) (recur))
+      (do
+        (println "==========================")
+        (round questions 10)
+        (recur))
       (println "See ya, wouldn't wanna be ya!"))))
 
 (defn -main
